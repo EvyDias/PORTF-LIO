6b74f3f96a6212a0d07ab2c43565c6ec2e1ea5c4
Evelyn Soares <evelyndias962@gmail.com>
2024-08-24 18:23:55 -0300

    Add files via upload

---
+++ PORTFÓLIO/teste java.js
@@ -0,0 +1,9 @@
+ano_atual = 2024;
+
+
+data_d_nascimento = prompt("digite o ano em que nasceu");
+
+idade = ano_atual - data_d_nascimento
+
+console.log("calcular idade...");
+console.log("A sua idade é" + idade);
