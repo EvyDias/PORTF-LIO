b1b715512d9d431dfda0fde6615a5aa6ba5ecd34
Evelyn Soares <evelyndias962@gmail.com>
2024-08-26 14:36:15 -0300

    Delete PORTFÓLIO/teste java.js

--- PORTFÓLIO/teste java.js
+++
@@ -1,9 +0,0 @@
-ano_atual = 2024;
-
-
-data_d_nascimento = prompt("digite o ano em que nasceu");
-
-idade = ano_atual - data_d_nascimento
-
-console.log("calcular idade...");
-console.log("A sua idade é" + idade);
